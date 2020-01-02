d405ccda90f8c4883722e323f354b3998e59e5ee
Andrzej Dybowski <dybowski@andrzejd.pl>
2020-01-02 18:15:41 +0100

    add routing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,51 +1,13 @@
 <template>
-    <div class="content">
-        <div>
-            <h1>Andrzej Dybowski</h1>
-        </div>
-        <div id="links">
-            <ul>
-                <li>
-                    <a href="https://www.linkedin.com/in/andrzej-dybowski/" target="_blank">
-                        <font-awesome-icon :icon="['fab', 'linkedin']"/>
-                        LinkedIn
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/andrzejd-pl/" target="_blank">
-                        <font-awesome-icon :icon="['fab', 'github']"/>
-                        GitHub
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/andrzejd-pl/portfolio" target="_blank">
-                        Portfolio
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+  <div class="content"><p>Hello home!!</p></div>
 </template>
 
 <script>
-    export default {
-        name: 'HomePage',
-        components: {},
-    }
+export default {
+  name: "HomePage",
+  components: {},
+};
 </script>
 
 <style scoped>
-    ul {
-        list-style-type: none;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 5px;
-    }
-
-    a {
-        text-decoration: none;
-        color: #42b983;
-    }
 </style>
